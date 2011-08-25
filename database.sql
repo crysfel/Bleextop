d539ce8aac16e5fee1c0784196d3bc218159ca48
Crysfel <crysfel@bleext.com>
2011-08-25 14:20:57 -0500

    Added permissions to roles-applications-users

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 10, 2011 at 11:40 PM
+-- Generation Time: Aug 25, 2011 at 07:19 PM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.1
 
@@ -36,18 +36,19 @@ CREATE TABLE IF NOT EXISTS `applications` (
   `date_updated` datetime NOT NULL,
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`application_k`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `applications`
 --
 
 INSERT INTO `applications` (`application_k`, `application_parent_k`, `name`, `description`, `klass`, `configurations`, `date_created`, `date_updated`, `active`) VALUES
-(1, NULL, 'Administration', 'Administration folder', '', '{"iconCls":"","width":800,"height":480,"shorcutIconCls":""}', '2011-08-09 11:03:53', '2011-08-09 11:03:53', 1),
-(2, 1, 'Applications', 'Applications catalog', 'Bleext.modules.catalogs.applications.controller.Application', '{\r\n   "iconCls":"applications-icon"\r\n}', '2011-08-09 11:03:53', '2011-08-09 11:03:53', 1),
+(1, NULL, 'Administration', 'Administration folder', '', '{"iconCls":"","width":800,"height":480,"shorcutIconCls":""}', '2011-08-09 11:03:53', '2011-08-12 06:39:07', 1),
+(2, 1, 'Applications', 'Applications catalog', 'Bleext.modules.catalogs.applications.controller.Application', '{"iconCls":"applications-icon","width":800,"height":480,"shorcutIconCls":""}', '2011-08-09 11:03:53', '2011-08-24 12:00:31', 1),
 (3, 1, 'Roles', 'Roles catalog', 'Bleext.modules.catalogs.roles.controller.Roles', '{"iconCls":"roles-icon","width":800,"height":480,"shorcutIconCls":""}', '2011-08-09 11:03:53', '2011-08-09 11:16:10', 1),
 (4, 1, 'Users', 'Users module', 'Bleext.modules.catalogs.users.controller.Users', '{"iconCls":"users-icon","shorcutIconCls":"roles-app-shorcut-icon","width":800,"height":480}', '2011-08-09 11:03:53', '2011-08-09 10:57:29', 1),
-(6, 1, 'Privileges', 'This module allow you to set the privileges to the roles and applications', 'Bleext.modules.catalogs.privileges.controller.Privileges', '{"iconCls":"permissions-icon","width":800,"height":480,"shorcutIconCls":""}', '2011-08-09 11:03:53', '2011-08-09 11:19:43', 1);
+(6, 1, 'Groups', 'This module allow you to add users to the groups', 'Bleext.modules.security.groups.controller.Groups', '{"iconCls":"groups-icon-16","width":850,"height":480,"shorcutIconCls":"roles-app-shorcut-icon"}', '2011-08-09 11:03:53', '2011-08-25 07:14:10', 1),
+(7, 1, 'Permissions', 'Module to manage the permissions assigned to roles and applications.', 'Bleext.modules.security.permissions.controller.Permission', '{"iconCls":"permissions-icon-16","width":800,"height":480,"shorcutIconCls":""}', '2011-08-16 05:47:39', '2011-08-25 07:13:40', 1);
 
 -- --------------------------------------------------------
 
@@ -63,16 +64,49 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   `description` text NOT NULL,
   PRIMARY KEY (`permission_k`),
   KEY `application_k` (`application_k`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=44 ;
 
 --
 -- Dumping data for table `permissions`
 --
 
 INSERT INTO `permissions` (`permission_k`, `application_k`, `action`, `name`, `description`) VALUES
-(1, 1, 'folder_menu_read', 'Read content access', 'Permission to show menu'),
-(2, 2, 'application_read', 'Read content access', 'Permission to read all the applications'),
-(3, 6, 'permissions_read', 'Read permissions', 'Allow users to read permissions, this module should be visible only for administrators');
+(1, 1, 'access', 'Access', 'Permission to show menu'),
+(2, 2, 'access', 'Access', 'Permission to read all the applications'),
+(3, 7, 'access', 'Access', 'Allow users to read permissions, this module should be visible only for administrators'),
+(4, 2, 'view', 'View', ''),
+(5, 2, 'list', 'List', ''),
+(6, 2, 'edit', 'Edit', ''),
+(7, 2, 'delete', 'Delete', ''),
+(8, 2, 'export', 'Export', ''),
+(9, 2, 'print', 'Print', ''),
+(10, 6, 'access', 'Access', ''),
+(11, 6, 'view', 'View', ''),
+(12, 6, 'list', 'List', ''),
+(13, 6, 'edit', 'Edit', ''),
+(14, 6, 'delete', 'Delete', ''),
+(15, 6, 'export', 'Export', ''),
+(16, 6, 'print', 'Print', ''),
+(17, 7, 'view', 'View', ''),
+(18, 7, 'list', 'List', ''),
+(19, 7, 'edit', 'Edit', ''),
+(20, 7, 'delete', 'Delete', ''),
+(21, 7, 'export', 'Export', ''),
+(22, 7, 'print', 'Print', ''),
+(23, 4, 'access', 'Access', ''),
+(24, 4, 'view', 'View', ''),
+(25, 4, 'list', 'List', ''),
+(26, 4, 'edit', 'Edit', ''),
+(27, 4, 'delete', 'Delete', ''),
+(28, 4, 'export', 'Export', ''),
+(29, 4, 'print', 'Print', ''),
+(30, 3, 'access', 'Access', ''),
+(31, 3, 'view', 'View', ''),
+(32, 3, 'list', 'List', ''),
+(33, 3, 'edit', 'Edit', ''),
+(34, 3, 'delete', 'Delete', ''),
+(35, 3, 'export', 'Export', ''),
+(36, 3, 'print', 'Print', '');
 
 -- --------------------------------------------------------
 
@@ -111,7 +145,7 @@ CREATE TABLE IF NOT EXISTS `role_permissions` (
   PRIMARY KEY (`role_permission_k`),
   KEY `role_k` (`role_k`),
   KEY `permission_k` (`permission_k`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `role_permissions`
@@ -120,7 +154,8 @@ CREATE TABLE IF NOT EXISTS `role_permissions` (
 INSERT INTO `role_permissions` (`role_permission_k`, `role_k`, `permission_k`, `value`, `date_created`) VALUES
 (1, 1, 1, 1, '2011-08-05 17:35:39'),
 (2, 1, 2, 1, '2011-08-05 17:35:47'),
-(3, 1, 3, 1, '0000-00-00 00:00:00');
+(3, 1, 3, 1, '0000-00-00 00:00:00'),
+(4, 1, 10, 1, '2011-08-17 10:59:18');
 
 -- --------------------------------------------------------
 
@@ -197,8 +232,16 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
 
 INSERT INTO `user_roles` (`user_k`, `role_k`, `date_created`) VALUES
 (1, 1, '2011-08-05 17:36:09'),
-(2, 2, '0000-00-00 00:00:00'),
-(3, 2, '0000-00-00 00:00:00');
+(3, 3, '2011-08-13 12:47:07'),
+(2, 1, '2011-08-13 01:50:27'),
+(3, 2, '2011-08-14 03:22:54'),
+(2, 2, '2011-08-14 03:29:26'),
+(4, 2, '2011-08-14 03:29:33'),
+(2, 3, '2011-08-14 07:23:37'),
+(1, 3, '2011-08-16 03:40:41'),
+(1, 2, '2011-08-17 06:15:16'),
+(5, 3, '2011-08-17 06:15:42'),
+(5, 1, '2011-08-17 06:20:54');
 
 --
 -- Constraints for dumped tables
